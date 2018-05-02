b80e970165ecbd3b17e36a9e8638fc7d43e28ed5
Jake Sippy <jsippy@cs.washington.edu>
2018-05-01 23:02:33 -0700

    Added screenshot and created structures to save iterations

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Mandelbrot Set viewer in Python and C++
 
-- mandelbrot.cc contains the C++ implementation that automatically rerenders with increasing accuracy (requires SFML to compile).
+- mandelbrot.cc contains the C++ implementation that automatically rerenders the set with increasing accuracy (requires SFML to compile).
 - mandel_clicker.py is a proof on concept written in Python that allows the user to click to increase accuracy.
+
+![Pretty Fractal](./images/screenshot.png)
--- mandelbrot.cc
+++ mandelbrot.cc
@@ -2,8 +2,8 @@
 #include <iostream>
 #include <complex>
 
-#define W 300
-#define H 200
+#define W 720
+#define H 480
 
 #define R_START -2
 #define R_END    1
@@ -11,48 +11,68 @@
 #define I_START  1
 #define I_END   -1
 
+#define ITER_STEP 1
+
 typedef struct RgbColor {
     unsigned char r;
     unsigned char g;
     unsigned char b;
 } RgbColor;
 
-typedef struct HsvColor {
-    unsigned char h;
-    unsigned char s;
-    unsigned char v;
-} HsvColor;
-
 // computes the escape time of a given complex
 // number if it is less than max_iters
-sf::Uint8 mandelbrot(std::complex<float>, int);
+static sf::Uint8 mandelbrot(std::complex<double>, int);
+
+// method to continue the mandelbrot iterations
+// using arrays that save the current z and iteration
+static void continueMandel(std::complex<double>,
+    std::complex<double>*, int*, int);
+
+// make an array of complexes
+static std::complex<double>* makeComplexes();
+
+// computes the index (given a coordinate point)
+// for the arrays used
+static int getIndex(int, int);
 
 // Convert a greyscale 0-255 to a color
-RgbColor HueToRgb(sf::Uint8);
+static RgbColor HueToRgb(sf::Uint8);
+
 
 int main() {
   sf::RenderWindow window(sf::VideoMode(W, H), "SFML");
   sf::Texture texture;
-  sf::Uint8* pixels = new sf::Uint8[W*H*4];
+
+  auto pixels = new sf::Uint8[W*H*4];
+  auto iterations = new int[W*H*4];
+  auto zs = new std::complex<double>[W*H*4];
+  auto complexes = makeComplexes();
+
   texture.create(W, H);
   texture.setSmooth(true);
   sf::Sprite sprite(texture);
 
-  int curr_iter = 1;
+  int max_iters = 1;
   while (window.isOpen()) {
+    int i;                    // index
+
+    std::complex<double> c;   // the complex for each coord
+    std::complex<double> z;   // the current z for each c
+    int curr;                 // iterations current c has done
+
     for (int y = 0; y < H; y++) {
       for (int x = 0; x < W; x++) {
         // calculate the index
-        int i = 4 * (y * W + x);
+        i = getIndex(x, y);
 
-        // create the complex for this coord
-        float real = R_START + (1.0 * x / W) * (R_END - R_START);
-        float im   = I_START + (1.0 * y / H) * (I_END - I_START);
-        std::complex<float> c(real, im);
+        c = complexes[i];
+        z = zs[i];
+        curr = iterations[i];
 
         // find escape time of this complex
-        sf::Uint8 iters = mandelbrot(c, curr_iter);
-        sf::Uint8 hue = 255 - 255 * ((1.0 * iters) / curr_iter);
+        continueMandel(c, &z, &curr, max_iters);
+
+        sf::Uint8 hue = 255 - 255 * ((1.0 * curr) / max_iters);
 
         // colorized output
         RgbColor rgb = HueToRgb(hue);
@@ -71,9 +91,15 @@ int main() {
 
     sf::Event event;
     while (window.pollEvent(event)) {
-	if (event.type == sf::Event::Closed)
-	    window.close();
-    } 
+	if (event.type == sf::Event::Closed) {
+            delete[] complexes;
+            delete[] iterations;
+            delete[] zs;
+            delete[] pixels;
+            window.close();
+            return 0;
+        }
+    }
 
     window.clear();
     texture.update(pixels);
@@ -81,13 +107,17 @@ int main() {
     window.draw(sprite);
     window.display();
 
-    curr_iter++;
+    max_iters += ITER_STEP;
   }
+  delete[] complexes;
+  delete[] iterations;
+  delete[] zs;
+  delete[] pixels;
   return 0;
 }
 
-sf::Uint8 mandelbrot(std::complex<float> c, int max_iters) {
-  std::complex<float> z(0, 0);
+static sf::Uint8 mandelbrot(std::complex<double> c, int max_iters) {
+  std::complex<double> z(0, 0);
   sf::Uint8 iters = 0;
   while (iters < max_iters && std::abs(z) < 2) {
     z = z*z + c;
@@ -96,7 +126,38 @@ sf::Uint8 mandelbrot(std::complex<float> c, int max_iters) {
   return iters;
 }
 
-RgbColor HueToRgb(sf::Uint8 hue) {
+static void continueMandel(std::complex<double> c,
+    std::complex<double>* z, int* iters, int max_iters) {
+
+  while (*iters < max_iters && std::abs(*z) < 2) {
+    *z = (*z) * (*z) + c;
+    (*iters)++;
+  }
+  return;
+}
+
+static std::complex<double>* makeComplexes() {
+  auto result = new std::complex<double>[W*H*4];
+  int i;
+  double real, im;
+
+  for (int y = 0; y < H; y++) {
+    for (int x = 0; x < W; x++) {
+      i = getIndex(x, y);
+      real = R_START + ((double) x / W) * (R_END - R_START);
+      im = I_START + ((double) y / H) * (I_END - I_START);
+      std::complex<double> c(real, im);
+      result[i] = c;
+    }
+  }
+  return result;
+}
+
+static int getIndex(const int x, const int y) {
+  return 4 * (y * W + x);
+}
+
+static RgbColor HueToRgb(sf::Uint8 hue) {
     RgbColor rgb;
 
     // saturation and value forced to be max
